ecea5b0c496e465aa51cc40c9f6f330d85b84aa7
Catzzz <chuneleu@gmail.com>
2021-06-02 09:54:55 -0700

    add docker setup

--- server.js
+++ server.js
@@ -8,24 +8,27 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const morgan = require('morgan');
 
 const db = knex({
   // connect to your own database here
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+  connection: process.env.POSTGRES_URI
+  // connection: {
+  //   host : '127.0.0.1',
+  //   user : 'postgres',
+  //   password : '',
+  //   database : 'smart-brain'
+  // }
 });
 
 const app = express();
-
+app.use(morgan('combined'));
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+// app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send("It's working") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
