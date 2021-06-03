60c6656a3ac6ae29dc27dd8dece0bdd655671b92
Catzzz <chuneleu@gmail.com>
2021-06-03 10:42:47 -0700

    getAuthToken funcion implement

--- controllers/signin.js
+++ controllers/signin.js
@@ -38,8 +38,15 @@ const handleSignin = (db, bcrypt, req, res) => {
     .catch((err) => Promise.reject("wrong credentials"));
 };
 
-const getAuthTokenId = () => {
-  console.log("auth ok");
+const getAuthTokenId = (req,res) => {
+  const {authorization} = req.headers;
+  return redisClient.get(authorization, (err, reply)=> {
+    if(err || !reply) {
+      return res.status(400).json('Unauthorized');
+    }
+    return res.json({id:reply})
+  })
+
 };
 
 const signToken = (email) => {
@@ -60,9 +67,10 @@ const createSessions = (user) => {
 };
 
 
+
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
-  return authorization ? getAuthTokenId() : 
+  return authorization ? getAuthTokenId(req,res) : 
   handleSignin(db, bcrypt, req, res)
   .then(data => {
     return data.id && data.email ? createSessions(data) : Promise.reject(data)
